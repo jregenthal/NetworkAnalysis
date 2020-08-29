c47492593f9ae66b531fd63abace4ca83a1c5bcb
jannikspiess <64558434+jannikspiess@users.noreply.github.com>
2020-08-29 10:37:28 +0200

    Final changes Jannik

--- Rosenblatt2020/NetworkAnalysis_FinalProject_Plotting.R
+++ Rosenblatt2020/NetworkAnalysis_FinalProject_Plotting.R
@@ -1,13 +1,13 @@
 #### Presettings ####
 
-# Load packages
+# Load required packages
 
 library(ggplot2)
 library(tidyverse)
 library(dplyr)
 
 
-# Import dataframes
+# Import dataframes containing results from outbreak simulations
 
 data_between <- read.csv("data_between.csv", sep = ";")
 data_degree <- read.csv("data_degree.csv", sep = ";")
@@ -17,7 +17,7 @@ data_page <- read.csv("data_page.csv", sep = ";")
 
 #### Transformation of data #### 
 
-# Define function to transform data
+# Define function to transform data 
 
 transform_data <- function(data) {
   data_trans <- data %>%
@@ -29,7 +29,8 @@ transform_data <- function(data) {
   return(data_trans)
 }
 
-# Generate transformed dataframes for each centrality measure using function
+# Generate transformed dataframes for each centrality measure using function 
+# transform_data
 
 data_between_trans <- transform_data(data_between)
 data_degree_trans <- transform_data(data_degree)
@@ -37,36 +38,12 @@ data_eigen_trans <- transform_data(data_eigen)
 data_page_trans <- transform_data(data_page)
 
 
-#### Robustness_Nie + Robustness_Ros (Same scale) ####
+#### Comparison of the original measures ####
 
-# Define function to plot both robustness measures on the same scale
+# Define function to plot the measures proposed by Martin and Niemeyer (2019)
+# and Rosenblatt et al. (2020)
 
-plot_same_scale <- function(data_trans, title) {
-  # Convert dataframe in a format that is suitable for plotting with ggplot
-  data_trans <- data_trans %>%
-    mutate(Type = "NULL")
-  data_plot <- rbind(
-  data_trans %>%
-    select(p, mean = mean_Nie, std = std_Nie, Type) %>%
-    mutate(Type = "Niemeyer"), 
-  data_trans %>%
-    select(p, mean= mean_Ros, std = std_Ros, Type) %>%
-    mutate(Type = "Rosenblatt"))
-  # Generate plot 
-  ggplot(data_plot, aes(x = p, y = mean, color = Type)) + 
-    geom_point() + geom_line() +
-    geom_errorbar(aes(ymin = mean-std, ymax = mean+std), 
-                  width = 0.02) + 
-    scale_y_continuous("Mean robustness") +
-    scale_x_continuous("Error level p") +
-    scale_color_manual(values = c("#C3A951", "#5683B0")) +
-    labs(title = title) +
-    theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5))
-}
-
-
-plot_same_scale <- function(data_trans, title) {
+plot <- function(data_trans, title) {
   # Convert dataframe in a format that is suitable for plotting with ggplot
   data_trans <- data_trans %>%
     mutate(Type = "NULL")
@@ -82,104 +59,58 @@ plot_same_scale <- function(data_trans, title) {
     geom_point() + geom_line() +
     geom_errorbar(aes(ymin = mean-std, ymax = mean+std), 
                   width = 0.02) + 
-    scale_y_continuous("Robustness of Centrality Measure", 
-                       sec.axis = sec_axis(~.*1, name = "Difference in Outbreak Size vs. Random Immunization")) +
+    scale_y_continuous("Difference in Outbreak Size vs. Random Immunization", 
+                       sec.axis = sec_axis(~.*1, name = "Robustness of Centrality Measure")) +
     scale_x_continuous("Proportion of missing nodes p") +
-    scale_color_manual(values = c("#C3A951", "#5683B0")) +
+    scale_color_manual(values = c("red", "blue")) +
     labs(title = title) +
     theme_bw() +
-    theme(axis.title.y.left = element_text(colour = "#C3A951"),
-          axis.title.y.right = element_text(colour = "#5683B0"),
+    theme(axis.title.y.left = element_text(colour = "blue"),
+          axis.title.y.right = element_text(colour = "red"),
           plot.title = element_text(hjust = 0.5),
           legend.position = "none")
 }
 
+# Generate plots for each centrality measure using function plot
 
-# Generate plots for each centrality measure using function
-
-
-plot_same_scale(data_between_trans, "Betweenness Centrality")
-plot_same_scale(data_degree_trans, "Degree Centrality")
-plot_same_scale(data_eigen_trans, "Eigenvalue Centrality")
-plot_same_scale(data_page_trans, "Pagerank")
+plot(data_between_trans, "Betweenness Centrality")
+plot(data_degree_trans, "Degree Centrality")
+plot(data_eigen_trans, "Eigenvector Centrality")
+plot(data_page_trans, "Pagerank")
 
 
-#### Robustness_Nie + Robustness_Ros (Different scale) ####
+#### Traditional Robustness ####
 
-# Define function to plot both robustness measures on different scales
-
-plot_different_scale <- function(data_trans, title) {
-  # Define normalizing constant a for robustness measure by Rosenblatt
-  a <- max(data_trans$mean_Ros)
-  # Convert dataframe in a format that is suitable for plotting with ggplot
-  data_trans <- data_trans %>%
-    mutate(Type = "NULL")
-  data_plot <- rbind(
-    data_trans %>%
-      select(p, mean_norm = mean_Nie, std_norm = std_Nie, Type) %>%
-      mutate(Type = "Niemeyer"), 
-    data_trans %>%
-      mutate(mean_Ros = mean_Ros/a, std_Ros = std_Ros/a) %>%
-      select(p, mean_norm = mean_Ros, std_norm = std_Ros, Type) %>%
-      mutate(Type = "Rosenblatt")) 
-  
-  # Generate plot 
-  ggplot(data_plot, aes(x = p, y = mean_norm, color = Type)) + 
-    geom_point() + geom_line() +
-    geom_errorbar(aes(ymin = mean_norm-std_norm, ymax = mean_norm+std_norm), 
-                  width = 0.02) + 
-    scale_y_continuous("Robustness of Centrality Measure", 
-                       breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1),
-                       sec.axis = sec_axis(~.*a, name = "Difference in Outbreak Size vs. Random Immunization",
-                                           breaks = c(0.05, 0.10, 0.15, 0.20))) +
-    scale_x_continuous("Proportion of missing nodes p") +
-    scale_color_manual(values = c("#C3A951", "#5683B0")) +
-    labs(title = title) +
-    theme_bw() +
-    theme(axis.title.y.left = element_text(colour = "#C3A951"),
-          axis.title.y.right = element_text(colour = "#5683B0"),
-          plot.title = element_text(hjust = 0.5),
-          legend.position = "none")
-}
-
-# Generate plots for each centrality measure using function
-
-plot_different_scale(data_between_trans, "Betweenness Centrality (different scale)")
-plot_different_scale(data_degree_trans, "Degree Centrality (different scale)")
-plot_different_scale(data_eigen_trans, "Eigenvalue Centrality (different scale)")
-plot_different_scale(data_page_trans, "Pagerank (different scale)")
-
-
-#### Robustness_Nie (all centrality measures) ####
-
-# Generate transformed dataframe
+# Define function to transform data 
 
 transform_Nie <- function(data_trans, centrality) {
   data_trans <- data_trans %>%
     mutate(Centrality = centrality) %>%
-  select(p, mean = mean_Nie, std = std_Nie, Centrality)
+    select(p, mean = mean_Nie, std = std_Nie, Centrality)
 }
+
+# Generate transformed dataframe using function transform_Nie
 
 data_Nie <- rbind(transform_Nie(data_between_trans, "Betweenness"),
                   transform_Nie(data_degree_trans, "Degree"),
-                  transform_Nie(data_eigen_trans, "Eigenvalue"),
+                  transform_Nie(data_eigen_trans, "Eigenvector"),
                   transform_Nie(data_page_trans, "Pagerank"))
 
-# Generate plot
+# Generate plot with traditional robustness for all centrality measures
 
 ggplot(data_Nie, aes(x = p, y = mean, color = Centrality)) + 
   geom_point() + geom_line() +
   scale_y_continuous("Robustness of Centrality Measure") +
   scale_x_continuous("Proportion of missing nodes p") +
-  scale_color_manual(values = c("#C3A951", "#5683B0", "#8C6B88", "#66ACA7")) +
-  labs(title = "Robustness Niemeyer (All Centralities)") +
+  scale_color_manual(values = c("red", "blue", "green", "yellow")) +
+  labs(title = "Traditional Robustness") +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5))
 
 
-#### Robustness_Ros (all centrality measures) ####
+#### Immunization Effectiveness & Immunization Robustness ####
 
-# Generate transformed dataframe
+# Define function to transform data 
 
 transform_Ros <- function(data_trans, centrality) {
   data_trans <- data_trans %>%
@@ -187,21 +118,28 @@ transform_Ros <- function(data_trans, centrality) {
     select(p, mean = mean_Ros, std = std_Ros, Centrality)
 }
 
+# Generate transformed dataframe using function transform_Ros
+
 data_Ros <- rbind(transform_Ros(data_between_trans, "Betweenness"),
                   transform_Ros(data_degree_trans, "Degree"),
-                  transform_Ros(data_eigen_trans, "Eigenvalue"),
+                  transform_Ros(data_eigen_trans, "Eigenvector"),
                   transform_Ros(data_page_trans, "Pagerank"))
 
-# Generate plot
+# Generate plot with immunization effectiveness for all centrality measures
 
 ggplot(data_Ros, aes(x = p, y = mean, color = Centrality)) + 
   geom_point() + geom_line() +
   scale_y_continuous("Difference in Outbreak Size vs. Random Immunization") +
   scale_x_continuous("Proportion of missing nodes p") +
-  scale_color_manual(values = c("#C3A951", "#5683B0", "#8C6B88", "#66ACA7")) +
-  labs(title = "Robustness Rosenblatt (All Centralities)") +
+  scale_color_manual(values = c("red", "blue", "green", "yellow")) +
+  labs(title = "Immunization Effectiveness") +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5))
+
+# Generate plot with immunization robustness for all centrality measures
+
+
+
 
 
 
